d5e43b0b1c60ef27e3ba7fbd4b1ddfe26ed2c5bf
Danilo Gonçalves Pinheiro <124277720+DaniloPin@users.noreply.github.com>
2025-06-23 19:38:22 -0300

    Add files via upload

---
+++ InformacaoTecnica.cs
@@ -0,0 +1,9 @@
+﻿public class InformacaoTecnica 
+{
+    public decimal TamanhoMB {  get; set; }
+    public string SistemaOperacional { get; set; }
+    public InformacaoTecnica(decimal tamanhoMB, string sistemaOperacional) {
+        TamanhoMB = tamanhoMB;
+        SistemaOperacional = sistemaOperacional;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿InformacaoTecnica info = new InformacaoTecnica(1500, "Windows/Mac");
+ProdutoDigital produto = new ProdutoDigital("Photoshop", 89.99, info);
+produto.ExibirDetalhes();
